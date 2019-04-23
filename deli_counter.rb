4f1d7fb4b7cfe328c2afb94afdde867d13162fa3
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-23 01:36:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(place)
   else
     current_line= "The line is currently:"
     place.each.with_index(1) do |person, index|
-      current_line << " #{index}. #{peron}"
+      current_line << " #{index}. #{person}"
   end
   puts current_line
 end
